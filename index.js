5cc6e2be92517003c1852f51cf153b5c5f9d9ab9
Malachi <malachimart@gmail.com>
2016-05-26 15:12:27 -0500

    created Label react component, created file for rendering when no Window object is present

---
+++ index.js
@@ -0,0 +1,72 @@
+import { Children, PropTypes } from 'react';
+import { render, unmountComponentAtNode} from 'react-dom'
+import { Map, label } from 'leaflet'
+
+import MapComponent from './MapComponent'
+
+export default class Label extends MapComponent {
+  static propTypes = {
+    children: PropTypes.node,
+    map: PropTypes.instanceOf(Map),
+    popupContainer: PropTypes.object,
+    position: latlngType    
+  }
+  
+  componentWillMount() {
+    super.componentWillMount()
+    const { children: _children, map: _map, labelContainer, ...props } = this.props
+    this.leafletElement = label(props, labelContainer);
+    this.leafletElement.on('open', ::this.renderLabelContent)
+    this.leafletElement.on('close', ::this.removeLabelContent)    
+  }
+
+  componentDidMount() {
+    const { map, labelContainer, position } = this.props
+    const el = this.leafletElement
+  
+   if( labelContainer ) {
+     labelContainer.bindLabel(el)
+   }
+   el.openOn(map)
+   
+  }
+  
+  componentDidUpdate (prevProps) {
+    const { position } = this.props
+    if(this.leafletElement._isOpen) {
+      this.renderLabelContent()
+    }
+  }
+  
+  componentWillUnmount() {
+    super.componentWillUnmount()
+    this.removeLabelContent()
+    if(this.leafletElement._map !== null){
+      this.props.map.removeLayer(this.leafletElement)
+    }   
+  }
+  
+  renderLabelContent() {
+    if(this.props.children) {
+      render(
+        Children.only(this.props.children),
+        this.leafletElement._contentNode
+      )
+      
+      this.leafletElement._updateLayout()
+      this.leafletElement._updatePosition()
+    }
+    else {
+      this.removeLabelContent()
+    }
+  }
+  
+  removeLabelContent() {
+    if (this.leafletElement._contentNode) {
+      unmountComponentAtNode(this.leafletElement._contentNode)
+    }
+  }
+  render() {
+    return null
+  }
+} 
